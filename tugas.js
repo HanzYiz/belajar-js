10767068c82a08d4a1845900cc5003eb6e7df758
HanzYiz <maulanafarhan05@gmail.com>
2024-02-04 20:43:37 +0700

    Tugas GDSC Task 1

--- tugas.js
+++ tugas.js
@@ -1,27 +1,66 @@
-//task 1
+/*
 
+      Tugas GDSC
+      Nama : Farhan Maulana
+      Kelas : F1A6
+      Npm : 202310715270
+  
+   Copyright by Farhan Maulana
+
+   sosmed :
+   IG : farhanmaulana.12
+   GitHub : https://github.com/HanzYiz
+*/
+
+
+//task 1
+console.log(`Task 1`);
+console.log(` `);
 const nama = `Farhan`;
 const umur = `19`;
 const posisi = `manager`;
 let kehadiran = `masuk`;
 
+if (!nama){
+  console.log(`Nama harus di isi`);
+  return;
+}
+
+if (!umur){
+  console.log(`Umur harus di isi`);
+  return;
+}
+
+if (!posisi){
+  console.log(`Posisi harus di isi`);
+  return;
+}
+
+if (!kehadiran){
+  console.log(`kehadiran harus di isi`);
+}
 console.log(`Nama karyawan ialah : ${nama}`);
-
+console.log(` `);
 console.log(`Umurnya adalah : ${umur} tahun`);
-
+console.log(` `);
 console.log(`Posisi kerja saat ini adalah : ${posisi}`);
+console.log(` `);
 //task 2
+console.log(`Task 2`);
+console.log(` `);
 if(posisi == "karyawan"){
     let jam = 7;
     jam ++
 
-    console.log(`Jam kerja karyawan adalah ${jam}`)
+    console.log(`Jam kerja karyawan adalah ${jam} jam`)
+    console.log(` `);
 }
 else if (posisi == `manager`){
     jam = 10;
     jam += 2
 
-    console.log(`Jam kerja Manager adalah ${jam}`)
+    console.log(`Jam kerja Manager adalah ${jam} jam`)
+    console.log(` `);
 }
 else{
 
@@ -29,13 +68,17 @@ else{
 }
 
 //task 3
+console.log(`Task 3`);
+console.log(` `);
 if (kehadiran === 'masuk'){
 
    console.log (`${nama} telah masuk, terima kasih.`);
+   console.log (` `);
 }
 else if(kehadiran === `cuti`){
 
     console.log (`${nama} telah mengambil jatah cuti, terimakasih.`);
+    console.log(` `);
 }
 else{
 
@@ -43,7 +86,8 @@ else{
 }
 
 //task 4
-
+console.log(`Task 4`);
+console.log(` `);
 let day;
 
 switch (new Date().getDay()) {
@@ -73,15 +117,21 @@ switch (new Date().getDay()) {
 console.log(`hari ini adalah : ${day}`);
 if (day === 'Minggu') {
     console.log(`Hari ini ${nama} sedang libur karena hari ${day}.`);
+    console.log(` `)
 } else if (day === 'Senin' || day === 'Selasa' || day === 'Rabu' || day === 'Kamis' || day === 'Jumat') {
     console.log(`Hari ini ${day} ${nama} masuk sesuai jam kerja ${jam}.`);
+    console.log(` `)
 } else if (day === 'Sabtu') {
     console.log(`Hari ini ${nama} sedang libur karena hari ${day}.`);
+    console.log(` `)
 }
 
 //task 5
+console.log(`Task 5`);
+console.log(` `);
 const arraywarna = [`merah`, `kuning`, `hijau`, `biru`]
 
 for(let i = 0; i < arraywarna.length; i++){
     console.log(`${arraywarna[i]}`)
+    console.log(` `)
 }
